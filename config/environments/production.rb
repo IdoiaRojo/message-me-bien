4ee40e69b06c76be202d72215f8776b157480a29
idoia <idoia@ifeelonline.com>
2019-08-07 17:49:23 +0200

    chat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_cable.allowed_request_origins = ['https://message-me-bien.herokuapp.com/']
+  config.action_mailer.default_url_options = { host: 'https://message-me-bien.herokuapp.com/' }
 end
