7ddd0756550d65fcef60e0fcc2375ca1e228d864
idoia <idoia@ifeelonline.com>
2019-08-07 12:52:58 +0200

    feir

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,7 @@
 class ChatroomController < ApplicationController
     def index
-        
+        # @articles = Article.paginate(page: params[:page], per_page: 5)
+        @messages = Message.all
     end
     
     
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "idoia02", password: "password")
+User.create(username: "idoia03", password: "password")
+User.create(username: "idoia04", password: "password")
+User.create(username: "idoia05", password: "password")
+
+Message.create(body: "holiii", user_id: 2 )
+Message.create(body: "aloha", user_id: 3 )
+Message.create(body: "holaaaa", user_id: 4 )
