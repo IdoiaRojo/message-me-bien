eced0ff7b09ff5546d378165095f668d73c4ab11
idoia <idoia@ifeelonline.com>
2019-08-07 13:44:36 +0200

    madre miaaaaaa

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -5,4 +5,5 @@ class ChatroomController < ApplicationController
     end
     
     
+    
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,20 @@ class SessionsController < ApplicationController
         
     end
     def create
-        
+        user = User.find_by(username: params[:session][:username].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "Yujuuu bienvenida #{user.username}"
+            redirect_to root_path
+        else
+            flash.now[:error] = "Te has equivocado en algo ..."
+            render 'new'
+        end
+    end
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "¡¡Vuelve pronto porfaaa!!"
+        redirect_to login_path
     end
     
 end
