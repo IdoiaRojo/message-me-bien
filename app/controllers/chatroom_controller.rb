387c5d444599eabbb3579b3dd921356a068e959e
idoia <idoia@ifeelonline.com>
2019-08-07 16:17:23 +0200

    antes del chat

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,11 +1,14 @@
 class ChatroomController < ApplicationController
     before_action :require_user
-    
+
     def index
         # @articles = Article.paginate(page: params[:page], per_page: 5)
+        @message = Message.new
         @messages = Message.all
     end
     
+    def create
     
+    end
     
 end
